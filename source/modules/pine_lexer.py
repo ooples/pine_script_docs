d301cdd5302d9ee11014197d9a092678b106126c
Bychkov Timofey <77493664+tbychkovTV@users.noreply.github.com>
2021-09-22 11:47:42 +0300

    Added new entities to regexp
    An attempt was made
    Added new keywords to the Keywords regex and rewrote the Entities regex to include all currently existing functions and variables.

--- source/modules/pine_lexer.py
+++ source/modules/pine_lexer.py
@@ -13,9 +13,15 @@ class PinePygmentsLexer(RegexLexer):
             (r'(\.\d+|[0-9]+\.[0-9]*)([eE][-+]?[0-9]+)?', Number.Float),
             (r'\s+', Text.Whitespace),
             (r'//.*?$', Comment), 
-            (r'(for|if|else|var)\b', Keyword),
+            (r'(for|if|else|var|varip|while|switch|export|import|series|simple|float|int|bool|string|color|line|label|box|table)\b', Keyword),
             (r'(open|high|low|close|volume|time|hl2|hlc3|ohlc4)\b', Name.Constant), # Built-in series 'open', 'high', ...
-            (r'(indicator|strategy|plot|plotshape|plotchar|plotarrow|fill|hline|input)\b', Name.Entity), # Annotation function
+            (r'(bar_index|dayofmonth|hour|minute|month|na|second|time_close|time_tradingday|timenow|year|'
+             r'(alertcondition|barcolor|bgcolor|bool|fill|fixnan|float|hline|hour|indicator|int|library|'
+             r'nz|plot|plotarrow|plotbar|plotcandle|plotchar|plotshape|string|timestamp|weekofyear|'
+             r'(strategy|input|plot|hline|alert|color|dayofweek|dividends|earnings|label|line|splits|box|table)(\..+)?|'
+             r'(adjustment|barmerge|barstate|currency|dayofweek|display|extend|format|location|'
+             r'math|position|scale|session|shape|size|syminfo|ta|text|timeframe|xloc|yloc|array|'
+             r'request|str|ticker)\..+)\b', Name.Entity), # Built-in functions and variables
             (r'[\w\.]+', Name.Other),
             (r'\+|\-|\*|\/|\%|\=|\[|\]|and|or|not|\?|\:|\<|\>|\!', Operator),
             (r'\(|\)|\,', Punctuation),
